845c5ef8e9c54dee1c55b3e92fe830cf9bbba324
Ksawery <koperekjan4@gmail.com>
2025-05-26 21:39:46 +0200

    Komentarze #1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const readline = require('readline');
 
+// Definicja klasy Item
 class Item {
+    // Przyjmuje nazwe, wage w kg, poziom dziwności i informację o delikatności
     constructor(name, weightKg, weirdnessLevel, isFragile) 
     {
         this.name = name;
@@ -9,13 +11,14 @@ class Item {
         this.isFragile = isFragile;
     }
 
+    // Metoda zwraca opis przedmiotów w JSON
     describe()
     {
         return JSON.stringify({
-            nazwa: this.name,
-            waga_kg: this.weightKg,
-            poziom_dziwności: this.weirdnessLevel,
-            czy_delikatny: this.isFragile ? "tak" : "nie"
+            nazwa: this.name, // Nazwa przedmiotu
+            waga_kg: this.weightKg, // Waga w kilogramach
+            poziom_dziwności: this.weirdnessLevel, // Poziom dziwności
+            czy_delikatny: this.isFragile ? "tak" : "nie" // Informacja o delikatności
         } , null, 4);
     }
 }
